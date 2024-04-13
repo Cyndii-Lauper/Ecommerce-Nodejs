fab4d7bec0ac096944661bbd352982394db4c865
Minh <elevenine00@gmail.com>
2024-04-13 22:26:11 +0700

    - admin basic design -

---
+++ admin/pages/products.js
@@ -0,0 +1,11 @@
+
+import Layout from "@/components/Layout";
+import Link from "next/link";
+
+export default function Products() {
+    return (
+        <Layout>
+            <Link className="bg-blue-900 text-white rounded-md py-2 px-2" href={'/products'}>Thêm sản phẩm</Link>
+        </Layout>
+    );
+}
