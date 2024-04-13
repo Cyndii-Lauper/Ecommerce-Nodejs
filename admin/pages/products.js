74d831f3fce3844ea5d9cba75cdd95d71b31a643
Minh <elevenine00@gmail.com>
2024-04-13 23:16:49 +0700

    - admin: add product -

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Products() {
     return (
         <Layout>
-            <Link className="bg-blue-900 text-white rounded-md py-2 px-2" href={'/products'}>Thêm sản phẩm</Link>
+            <Link className="bg-blue-900 text-white rounded-md py-2 px-2" href={'/products/new'}>Thêm sản phẩm</Link>
         </Layout>
     );
 }
---
+++ admin/pages/products/new.js
@@ -0,0 +1,37 @@
+// import ProductForm from "@/components/ProductForm";
+import Layout from "@/components/Layout";
+import {useState} from "react";
+import axios from "axios";
+
+
+export default function NewProduct() {
+  const [title,setTitle] = useState('');
+  const [description,setDescription] = useState('');
+  // const [category,setCategory] = useState(assignedCategory || '');
+  const [price,setPrice] = useState('');
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    axios.post('/api/products', data);
+  }
+  return (
+    <Layout>
+      <form onSubmit={createProduct}>
+        <div className="form-group">
+          <label>Product Name</label>
+          <input type="text" className="form-control" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <textarea className="form-control" placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label>Price (in VND)</label>
+          <input type="number" className="form-control" placeholder="price" value={price} onChange={ev => setPrice(ev.target.value)}/>
+        </div>
+        <button type="submit" className="btn btn-primary">Save</button>
+      </form>
+      
+    </Layout>
+  );
+}
